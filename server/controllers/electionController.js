2e3ed847f41447e0a0210c86ae8bfcd089d13368
Suyogita Patel <suyogitapatel77@gmail.com>
2025-11-09 15:12:16 +0530

    Added complete backend server with models, controllers, middleware

--- server/controllers/electionController.js
+++ server/controllers/electionController.js
@@ -1,9 +1,61 @@
-// ====================ADD NEW ELECTION
+const {v4:uuid} = require("uuid");
+const cloudinary = require('../utils/cloudinary')
+const path = require("path")
+const HttpError = require("../models/ErrorModel")
+const ElectionModel = require('../models/electionModel')
+const CandidateModel = require('../models/candidateModel')
+
+
+// ==================== ADD NEW ELECTION
 // POST: api/elections
+
+
+
 // PROTECTED(only admin)
 
-const addElection = (req, res, next) =>{
-    res.json("Add Election")
+const addElection = async (req, res, next) =>{
+    try {
+        // only admin can add election
+    if(!req.user.isAdmin){
+        return next(new HttpError("Only an admin can perform this action.", 403))
+    }
+
+    const {title, discription} = req.body;
+    if(!title || !discription){
+        return next(new HttpError ("Fill all fields.", 422))
+    }
+    if(!req.files.thumbnail){
+        return next(new HttpError ("Choose the thumbnail.", 422))
+    }
+    const {thumbnail} = req.files;
+    //image should be less than 1mb
+    if(thumbnail.size > 1000000){
+        return next(new HttpError ("File size is too big. Should be less than 1mb"))
+    }
+    //rename the image
+    let fileName = thumbnail.name;
+    fileName = fileName.split(".")
+    fileName = fileName[0] + uuid() + "." + fileName[fileName.length - 1]
+    // upload file to uploads folder in project
+    await thumbnail.mv(path.join(__dirname, '..', 'upload', fileName), async (err) => {
+        if(err){
+            return next(HttpError(err))
+        }
+        // store image on cloudinary
+        const result = await cloudinary.uploader.upload(path.join(__dirname, '..', "upload", fileName), {resource_type: "image"})
+        if(!result.secure_url){
+            return next(new HttpError ("Couldn't upload image to cloudinary", 422))
+        }
+        // save election to db
+        const newElection = await ElectionModel.create({title, description, thumbnail: result.secure_url})
+        res.json(newElection)
+    })
+        
+    } catch (error) {
+        return next(new HttpError(error))
+        
+    }
+
 
 }
 
@@ -12,8 +64,14 @@ const addElection = (req, res, next) =>{
 // GET: api/elections
 // PROTECTED
 
-const getElections = (req, res, next) =>{
-    res.json("Get all Election")
+const getElections = async(req, res, next) =>{
+    try {
+        const elections = await ElectionModel.find();
+        res.status(200).json(elections)
+        
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 
 }
 
@@ -23,8 +81,14 @@ const getElections = (req, res, next) =>{
 // GET: api/elections/:id
 // PROTECTED
 
-const getElection = (req, res, next) =>{
-    res.json("Get Single Election")
+const getElection = async(req, res, next) =>{
+    try {
+        const {id} = req.params;
+        const election = await ElectionModel.findById(id)
+        res.status(200).json(election);
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 
 }
 
@@ -33,8 +97,16 @@ const getElection = (req, res, next) =>{
 // POST: api/elections/id/candidates
 // PROTECTED
 
-const getCandidatesOfElection = (req, res, next) =>{
-    res.json("Get Candidates of Election")
+const getCandidatesOfElection = async(req, res, next) =>{
+    try {
+        const {id} = req.params;
+        const candidates = await CandidateModel.find({election: id})
+        res.status(200).json(candidates)
+
+        
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 
 }
 
@@ -43,8 +115,15 @@ const getCandidatesOfElection = (req, res, next) =>{
 // POST: api/elections/:id/voters
 // PROTECTED
 
-const getElectionVoters = (req, res, next) =>{
-    res.json("Get Election Voters")
+const getElectionVoters = async (req, res, next) =>{
+    try {
+        const {id} = req.params;
+        const response = await ElectionModel.findById(id).populate('voters')
+        res.status(200).json(response.voters)
+        
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 
 }
 
@@ -56,8 +135,48 @@ const getElectionVoters = (req, res, next) =>{
 // PATCH: api/elections/:id
 // PROTECTED(only admin)
 
-const updateElection = (req, res, next) =>{
-    res.json("Update Election")
+const updateElection =async (req, res, next) =>{
+    try {
+        // only admin can add election
+    if(!req.user.isAdmin){
+        return next(new HttpEroor("Only an admin can perform this action.", 403))
+    }
+        const {id} = req.params;
+        const {title, discription} = req.body;
+        if(!title || !discription){
+            return next(new HttpError("Fill in all field", 422))
+        }
+        if(req.files.thumbnail){
+            const {thumbnail} = req.files;
+            // image should be less than 1mb
+            if(thumbnail.size > 1000000){
+                return next(new HttpError("Image size is too big, should be less than 1mb.", 422))
+            }
+            //rename the image
+    let fileName = thumbnail.name;
+    fileName = fileName.split(".")
+    fileName = fileName[0] + uuid() + "." + fileName[fileName.length - 1]
+    thumbnail.mv(path.join(__dirname, '..', "uploads", fileName), async (err) => {
+        if(err){
+            return next(new HttpError(err))
+        }
+        // store image on cloudinary
+        const result = await cloudinary.uploader.upload(path.join(__dirname, '..', 'uploads', fileName), {resource_type: "image"})
+        // check if cloudinary storage was successful
+        if(!result.secure_url){
+            return next(new HttpError("Image upload to cloudinary was not successful", 422))
+        }
+
+        await ElectionModel.findByIdAndUpdate(id, {title, description, thumbnail: result.secure_url})
+
+        res.json("Election updated successfully", 200)
+    })
+        }
+        
+        
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 
 }
 
@@ -68,8 +187,23 @@ const updateElection = (req, res, next) =>{
 // DELETE: api/elections/:id
 // PROTECTED(only admin)
 
-const removeElection = (req, res, next) =>{
-    res.json("Delete Election")
+const removeElection =async (req, res, next) =>{
+    try {
+        // only admin can add election
+    if(!req.user.isAdmin){
+        return next(new HttpEroor("Only an admin can perform this action.", 403))
+    }
+
+        const {id} = req.params;
+        await ElectionModel.findByIdAndDelete(id);
+        // delete candidates that belong to this election
+        await CandidateModel.deleteMany({election: id})
+        res.status(200).json("Election deleted successfully")
+        
+        
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 
 }
 
--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -3,6 +3,8 @@ const { registerVoter, loginVoter, getVoter } = require("../controllers/voterCon
 const {addElection,getElection, getElections, updateElection, removeElection, getCandidatesOfElection, getElectionVoters}= require("../controllers/electionController")
 const {addCandidate,getCandidate,removeCandidate,voteCandidate} = require("../controllers/candidateController")
 
+const authMiddleware = require("../middleware/authMiddleware")
+
 
 const router = Router();
 
@@ -13,24 +15,24 @@ router.post('/voters/register', registerVoter);
 router.post('/voters/login', loginVoter);
 
 // Get voter by ID
-router.get('/voters/:id', getVoter);
+router.get('/voters/:id',authMiddleware, getVoter);
 
 
 
-router.post('/elections', addElection)
-router.get('/elections', getElections)
-router.get('/elections/:id', getElection)
-router.delete('/elections/:id', removeElection)
-router.patch('/elections/:id', updateElection)
-router.get('/elections/:id/candidates', getCandidatesOfElection)
-router.get('/elections/:id/voters', getElectionVoters)
+router.post('/elections', authMiddleware, addElection)
+router.get('/elections',authMiddleware, getElections)
+router.get('/elections/:id', authMiddleware, getElection)
+router.delete('/elections/:id', authMiddleware, removeElection)
+router.patch('/elections/:id', authMiddleware, updateElection)
+router.get('/elections/:id/candidates', authMiddleware, getCandidatesOfElection)
+router.get('/elections/:id/voters',authMiddleware, getElectionVoters)
 
 
 
-router.post('/candidates' , addCandidate)
-router.get('/candidates/:id', getCandidate)
-router.delete('/candidates/:id', removeCandidate)
-router.patch('/candidates/:id', voteCandidate)
+router.post('/candidates' , authMiddleware, addCandidate)
+router.get('/candidates/:id', authMiddleware, getCandidate)
+router.delete('/candidates/:id', authMiddleware, removeCandidate)
+router.patch('/candidates/:id', authMiddleware, voteCandidate)
 
 
 module.exports = router;
